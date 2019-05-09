10f01c935cfbc078170ce7ef197755fbc65ecf8f
fradleybrancis <fradleybrancis@gmail.com>
2019-05-08 17:27:42 -0700

    create webpack config

---
+++ challenge_1/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path")
+
+module.exports = {
+  mode: "development",
+  entry: path.join(__dirname, "/client/index.jsx"),
+  devtool: "source-map",
+  output: {
+    path: path.join(__dirname, "/public"),
+    filename: "bundle.js",
+
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.jsx$/],
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env", "@babel/preset-react"],
+        }
+      }
+    ]
+  }
+}

de7502c22cc9fe37e5a1ee503af55eb19fd8047f
fradleybrancis <fradleybrancis@gmail.com>
2019-05-08 17:25:14 -0700

    create index.jsx and app component for basic react set-up

---
+++ challenge_1/client/Components/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      historicalEvents: [],
+    }
+  }
+
+  componentDidMount() {
+    
+  }
+
+  render() {
+    return (
+      <div>Hello World</div>
+    )
+  }
+}
+
+export default App;

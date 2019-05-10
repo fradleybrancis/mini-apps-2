5475fa1ee68d44e88bcd4a59b94aaea43c33c901
fradleybrancis <fradleybrancis@gmail.com>
2019-05-10 12:22:34 -0700

    create basic react/ express-server set-up

---
+++ challenge_3/client/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Okay = () => {
+  return <div>Hello From ReactVille</div>
+}
+
+ReactDOM.render(<Okay />, document.getElementById("app"));

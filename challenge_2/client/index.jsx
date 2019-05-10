fc94ac1505bf74fc577fe55f101bd2bb225fca81
fradleybrancis <fradleybrancis@gmail.com>
2019-05-09 19:17:38 -0700

    add App component to be rendered to DOM

--- challenge_2/client/index.jsx
+++ challenge_2/client/index.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./Components/App.jsx";
 
-const Okay = () => <div>Hello World from React</div>
-
-ReactDOM.render(<Okay />, document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById("app"));
 

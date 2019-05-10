180f7db4c96749635e1f83ade5e1a481bdbd0bda
fradleybrancis <fradleybrancis@gmail.com>
2019-05-09 19:21:39 -0700

    add get route to grab data from bitcoin api

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const path = require("path");
+const axios = require("axios");
 
 const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/prices", (req, res) => {
+  axios.get(`https://api.coindesk.com/v1/bpi/historical/close.json`)
+    .then(response => {
+      res.send(response.data);
+    })
+    .catch(error => {
+      console.log(error);
+      res.sendStatus(500)
+    })
+})
 
 app.listen(port, () => console.log(`listening on port ${port}`));
 

3c64d6c58140a9cb8203a4eba681f41477d7953f
fradleybrancis <fradleybrancis@gmail.com>
2019-05-11 15:36:32 -0700

    update totalScore function to work for spares

--- challenge_3/client/Components/App.jsx
+++ challenge_3/client/Components/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import ScorePad from "./ScorePad.jsx"
+import ScorePad from "./ScorePad.jsx";
+import ScoreBoard from "./ScoreBoard.jsx";
 
 class App extends React.Component {
   constructor(props) {
@@ -11,16 +12,28 @@ class App extends React.Component {
     this.updateTotalScore = this.updateTotalScore.bind(this);
   }
 
-  updateTotalScore(frameScore) {
+  componentDidUpdate(prevProps, prevState) {
+    const { frame } = this.state;
+    if (frame === 11) {
+      this.setState({ score: 0, frame: 0 });
+      alert("gg")
+    }
+  }
+
+  
+
+  updateTotalScore(frameScore, callback) {
     const { score, frame } = this.state;
-    this.setState({ score: score + frameScore, frame: frame + 1 });
+    this.setState({ score: score + frameScore, frame: frame + 1 }, () => callback);
   }
 
   render() {
+    const { score, frame } = this.state;
     return (
       <React.Fragment>
         <h1> Score Card </h1>
         <ScorePad updateTotalScore={ this.updateTotalScore }/>
+        <ScoreBoard score={ score } frame={ frame }/>
       </React.Fragment>
     )
   }
